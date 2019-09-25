7ced4c3d7807f275da531a331e94aa5ed3bb8cae
benjaminpodell <benpodellsmu@gmail.com>
2019-09-24 23:39:35 -0500

    Adjusted code to render table

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,102 +1,112 @@
-// Data not showing up in html and not sure why?
-// Variable storing the data in tableData
+// Storing the data.js info in the table data variable
 var tableData = data;
 
-// Referencing the the tbody element along with corresponding tbody feilds and buttons
+// Referencing the tbody and creating a function to add the data in the table rows inside a row variable
+var tbody = d3.select("tbody");
+function addingTableData(data) {
+    var row = tbody.append("tr");
 
-var tbody = document.querySelector("#tbody");
-var dateInput = document.querySelector("#datetime");
-var cityInput = document.querySelector("#city");
-var stateInput = document.querySelector("#state");
-var countryInput = document.querySelector("#country");
-var shapeInput = document.querySelector("#shape");
-var filterBtn = document.querySelector("#durationMinutes");
-var resetBtn = document.querySelector("#comments");
-
-// Event Listeners for the filter button and created reset button
-
-filterBtn.addEventListener("click", handleSearchButtonClick);
-resetBtn.addEventListener("click", handleResetButtonClick);
-
-// Renders the Table
-function manifestTable() {
-  tbody.innerHTML = "";
-  for (var i = 0; i < tableData.length; i++) {
-    
-    // Grabs the sighting object and filds
-    var sighting = tableData[i];
-    var fields = Object.keys(sighting);
-    
-    // Creates a new row in the tbody while setting index to be i + 
-    var row = tbody.insertRow(i);
-    for (var j = 0; j < fields.length; j++) {
-      
-      // For every field in the sighting object creates new cell at set its inner text to be the current value
-      var field = fields[j];
-      var cell = row.insertCell(j);
-      cell.innerText = sighting[field];
-    }
-  }
-}
-
-function handleSearchButtonClick() {
-  
-  // Goes through items with formatted user's search 
-
-  var filterDate = dateInput.value.trim();
-  if (filterDate != "") {
-    tableData = data.filter(function (sighting) {
-      var sightingDate = sighting.datetime;
-      return sightingDate === filterDate;
-    });
-  };
- 
-  var filterCity = cityInput.value.trim().toLowerCase();
-  if (filterCity != "") {
-    tableData = tableData.filter(function (sighting) {
-      var sightingCity = sighting.city;
-      return sightingCity === filterCity;
-    });
-  };
-  
-  var filterState = stateInput.value.trim().toLowerCase();
-  if (filterState != "") {
-    tableData = tableData.filter(function (sighting) {
-      var sightingState = sighting.state;
-      return sightingState === filterState;
-    });
-  };
-  
-  var filterCountry = countryInput.value.trim().toLowerCase();
-  if (filterCountry != "") {
-    tableData = tableData.filter(function (sighting) {
-      var sightingCountry = sighting.country;
-      return sightingCountry === filterCountry;
-    });
-  };
-  
-  var filterShape = shapeInput.value.trim().toLowerCase();
-  if (filterShape != "") {
-    tableData = tableData.filter(function (sighting) {
-      var sightingShape = sighting.shape;
-      return sightingShape === filterShape;
-    });
-  };
-  
-  manifestTable();
+    // Object loop that checks that keys and values are in same order every iteration
+    Object.entries(data).forEach(([key, value]) => {
+        row.append("td").text(value); 
+    })
 };
 
+// Appending all table rows and data 
+data.forEach(addingTableData);
 
-// Resets search data after search
+// Button variable that stores the filter button tag and calls d3 to select it
+var button = d3.select("#filter-btn");
 
-function handleResetButtonClick() {
-  tableData = data;
-  dateInput.value = "";
-  cityInput.value = "";
-  stateInput.value = "";
-  countryInput.value = "";
-  shapeInput.value = "";
-  manifestTable();
-}
+// Activating the button function and using prevent default to have button Oonly work clicked or user presses enter button
+button.on("click", function() {
+    d3.event.preventDefault();
+    
+    // Creating Variables that grab the datetime along with its value and creating a holder for the filtered data
+    var dateholder = d3.select("#datetime");
+    var timeholder = dateholder.property("value");
+    var filterholder = {};
 
-manifestTable();
+    // If statement that starts with checking if timeholder variable is not empty then do the following
+    if (timeholder !== "") {
+        filterholder.datetime = timeholder;
+    }
+    // Passing filters parameter, storing city, state, country, and shape filter value and placing the data as lowercased
+    if (filters) {
+        var citysliced = d3.select("#City-filter");
+        var city = citysliced.property("value").toLowerCase();
+
+        if (city !== "") {
+            filterholder.city = city;
+        }
+
+        var statesliced = d3.select("#State-filter");
+        var state = statesliced.property("value").toLowerCase();
+
+        if (state !== "") {
+            filterholder.state = state;
+        }
+
+        var countrysliced = d3.select("#Country-filter");
+        var country = countrysliced.property("value").toLowerCase();
+
+        if (country !== "") {
+            filterholder.country = country;
+        }
+
+        var shapesliced = d3.select("#Shape-filter");
+        var shape = shapesliced.property("value").toLowerCase();
+        
+        if (shape !== "") {
+            filterholder.shape = shape;
+        }
+    }
+
+    // Returning the data and making sure that the key matches the value for each data for proper mapping
+    var filtered = tableData.filter(obj => {
+        var propermatch = true;
+        Object.entries(filterholder).forEach(([key, value]) => {
+            propermatch = propermatch && (obj[key] === value);
+        });
+        return propermatch;
+    });
+    
+    // Checking to see what was returned in the console and looping through addingTableData
+    console.log(filtered);
+    tbody.html("");
+    filtered.forEach(addingTableData);
+});
+
+// Selecting the reset html tag and the more filter button tag and at this point passing the filters variable as false
+var resetattempt = d3.select("#reset-filter-btn");
+var filterattempt = d3.select("#more-filter-btn");
+var filters = false;
+
+// Allowing the folter button to be clicked and passing filters variable as true now
+filterattempt.on("click", function() {
+    d3.event.preventDefault();
+    filters = true;
+
+    // Use for loop to create additional filters
+    var filters = d3.select("#filters");
+    const filterlist = ["City", "State", "Country", "Shape"];
+
+    //Loop that takes place for every item in the filterlist that displays what is supposed to be filtered
+    filterlist.forEach(d => {
+        var newfilter_list = filters.append("li").attr("class","filter list-group-item");
+        newfilter_list.append("label").attr("for", d).text(`Enter a ${d}`);
+        newfilter_list.append("input").attr("class", "form-control")
+                             .attr("type", "text")
+                             .attr("id", `${d}-filter`);
+    });
+    filterattempt.style("display", "none");
+    resetattempt.style("display", "block");
+});
+
+// Reset button that is supposed to clear filters and displays all data
+resetattempt.on("click", function() {
+    d3.event.preventDefault();
+    var everyfilter = d3.selectAll("input")
+                       .property("value", "");
+    data.forEach(addingTableData);
+});
